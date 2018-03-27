f7ebcc308ccec8639ad73914ebf42aa4db50e97a
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-27 02:21:39 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,16 +4,15 @@ class Pokemon
   def initialize(data)
     @name = data[0]
     @type = data[1]
-    @db = data[2]
+    @id = data[2]
   end
   
   def self.save(name, type, db)
-    pokemon = Pokemon.new([name, type, db])
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
   
   def self.find(id, db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
-    temp = {}
+    temp = {id: pokemon[0], name: pokemon[1]}
   end
 end
