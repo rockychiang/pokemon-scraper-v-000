8aa7c13538b9289cb724d1d6f36f75911e1e640e
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-27 02:17:47 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,6 +13,7 @@ class Pokemon
   end
   
   def self.find(id, db)
-    db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    
   end
 end
